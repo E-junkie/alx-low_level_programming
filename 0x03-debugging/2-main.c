70605f404b1243de8812868e10c6a77a978eaef2
E-junkie <Lordzeus018@gmail.com>
2022-07-08 11:40:13 -0700

    Task

--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -3,7 +3,7 @@
 
 /**
  * main - prints the largest of 3 integers
- * Return: 0
+ *Return: 0
  */
 
 int main(void)
@@ -18,5 +18,6 @@ int main(void)
 	largest = largest_number(a, b, c);
 
 	printf("%d is the largest number\n", largest);
+
 	return (0);
 }
