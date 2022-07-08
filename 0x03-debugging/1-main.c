d1bd7b567c79238eb7372b22ab03094864b1115d
E-junkie <Lordzeus018@gmail.com>
2022-07-07 21:52:52 -0700

    T

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - cause an infinite loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("incoming infinite loop :(\n");
+
+	i = 0;
+/*
+ * do not add or removed any line of codd
+	while (i < 10)
+	{
+		putchar(i);
+	}
+*/
+	printf("infinite loop avoided! //o\n");
+
+	return (0);
+}
+
